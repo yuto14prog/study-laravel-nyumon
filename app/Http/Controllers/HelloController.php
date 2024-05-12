ab5bf94787670fcace2a5e4aa8c4c00787bbccc9
yuto14prog <yuto14prog@gmail.com>
2024-05-12 11:59:44 +0900

    Bladeを使わなくてもPHPでViewを作れる

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,26 +6,9 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    // __invoke()
-    // "__"で始まるものはPHPのマジックメソッド
-    // __invoke()はクラスのインスタンスを関数的に実行させるもの
-    public function __inVoke()
+    public function index()
     {
-        return <<<EOF
-            <html>
-                <head>
-                    <title>Hello</title>
-                    <style>
-                        // body { font-size:16pt; color:#999; }
-                        // h1 { fonst-size:100pt; text-align:right; color:#eee;
-                        //     margin:-40px 0px -50px 0px; }
-                    </style>
-                </head>
-                <body>
-                    <h1>Index</h1>
-                    <p>HelloControllerはシングルアクション・コントローラです。</p>
-                </body>
-            </html>
-    EOF;
+        $data = ['msg' => 'これはコントローラから渡されたメッセージです。'];
+        return view('hello.index', $data);
     }
 }
---
+++ resources/views/hello/index.php
@@ -0,0 +1,26 @@
+<html>
+
+<head>
+    <title>Hello</title>
+    <style>
+        /* body {
+            font-size: 16pt;
+            color: #999;
+        }
+
+        h1 {
+            font-size: 100pt;
+            text-align: right;
+            color: #eee;
+            margin: -40px 0px -50px 0px;
+        } */
+    </style>
+</head>
+
+<body>
+    <h1>Index</h1>
+    <p><?php echo $msg; ?></p>
+    <p><?php echo date('Y年n月j日'); ?></p>
+</body>
+
+</html>
