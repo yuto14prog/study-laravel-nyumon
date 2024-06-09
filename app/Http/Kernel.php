07f3a894ab6ac6c7f425f9143ad45dd23dbe8b2d
yuto14prog <yuto14prog@gmailcom>
2024-06-09 10:05:29 +0900

    グローバルミドルウェア

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends HttpKernel
      *
      * @var array<int, class-string|string>
      */
+    // グローバルミドルウェア（すべてのRouteに適用）
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
